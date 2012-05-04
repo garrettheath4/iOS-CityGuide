07bab5656da0e4060a3100b2993a9e87ae5f9c9a
Garrett Heath Koller <kollerg14@mail.wlu.edu>
2012-05-04 02:49:46 -0400

    Removed editing functionality

--- CityGuide/CGViewController.m
+++ CityGuide/CGViewController.m
@@ -27,7 +27,6 @@
     self.title = @"City Guide";
     CGAppDelegate *deligate = (CGAppDelegate *)[[UIApplication sharedApplication] delegate];
     cities = deligate.cities;
-    self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
 - (void)viewDidUnload
@@ -57,52 +56,6 @@
     return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
 }
 
--(void)setEditing:(BOOL)editing animated:(BOOL) animated {
-    if( editing != self.editing ) {
-        [super setEditing:editing animated:animated];
-        [tableView setEditing:editing animated:animated];
-        NSMutableArray *indices = [[NSMutableArray alloc] init];
-        for(int i=0; i < cities.count; i++ ) {
-            [indices addObject:[NSIndexPath indexPathForRow:i inSection:0]];
-        }
-        NSArray *lastIndex = [NSArray
-                              arrayWithObject:[NSIndexPath
-                                               indexPathForRow:cities.count inSection:0]];
-        if (editing == YES ) {
-            for(int i=0; i < cities.count; i++ ) {
-                UITableViewCell *cell =
-                [tableView
-                 cellForRowAtIndexPath:[indices objectAtIndex:i]];
-                [cell setSelectionStyle:UITableViewCellSelectionStyleNone];
-            }
-            [tableView insertRowsAtIndexPaths:lastIndex
-                             withRowAnimation:UITableViewRowAnimationLeft];
-        } else {
-            for(int i=0; i < cities.count; i++ ) {
-                UITableViewCell *cell =
-                [tableView
-                 cellForRowAtIndexPath:[indices objectAtIndex:i]];
-                [cell setSelectionStyle:UITableViewCellSelectionStyleBlue];
-            }
-            [tableView deleteRowsAtIndexPaths:lastIndex withRowAnimation:UITableViewRowAnimationLeft];
-        }
-    }
-}
-
-//- (void)setEditing:(BOOL)editing animated:(BOOL)animated {
-//    if (editing != self.editing) {
-//        [super setEditing:editing animated:animated];
-//        [tableView setEditing:editing animated:animated];
-//        
-//        NSArray *indexes = [NSArray arrayWithObject:[NSIndexPath indexPathForRow:cities.count inSection:0]];
-//        if (editing == YES) {
-//            [tableView insertRowsAtIndexPaths:indexes withRowAnimation:UITableViewRowAnimationLeft];
-//        } else {
-//            [tableView deleteRowsAtIndexPaths:indexes withRowAnimation:UITableViewRowAnimationLeft];
-//        }
-//    }
-//}
-
 #pragma mark UITableViewDataSource Methods
 
 - (UITableViewCell *)tableView:(UITableView *)tv cellForRowAtIndexPath:(NSIndexPath *)indexPath {
@@ -116,14 +69,6 @@
         City *thisCity = [cities objectAtIndex:indexPath.row];
         cell.textLabel.text = thisCity.cityName;
         cell.textLabel.textColor = [UIColor blackColor];
-        cell.editingAccessoryType = UITableViewCellAccessoryNone;
-        if (self.editing) {
-            [cell setSelectionStyle:UITableViewCellSelectionStyleNone];
-        }
-    } else {
-        cell.textLabel.text = @"Add New City...";
-        cell.textLabel.textColor = [UIColor lightGrayColor];
-        cell.editingAccessoryType = UITableViewCellAccessoryDisclosureIndicator;
     }
     
     return cell;
@@ -131,17 +76,7 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     NSInteger count = cities.count;
-    if (self.editing) {
-        count = count+1;
-    }
     return count;
-}
-
-- (void)tableView:(UITableView *)tv commitEditingStyle:(UITableViewCellEditingStyle)editing forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editing == UITableViewCellEditingStyleDelete) {
-        [cities removeObjectAtIndex:indexPath.row];
-        [tv deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationLeft];
-    }
 }
 
 #pragma mark UITableViewDelegate Methods
@@ -149,23 +84,11 @@
 - (void)tableView:(UITableView *)tv didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     CGAppDelegate *delegate = (CGAppDelegate *)[[UIApplication sharedApplication] delegate];
     
-    if (indexPath.row < cities.count && !self.editing) {
+    if (indexPath.row < cities.count) {
         CityController *city = [[CityController alloc] initWithIndexPath:indexPath];
         [delegate.navController pushViewController:city animated:YES];
     }
-    if (indexPath.row == cities.count && self.editing) {
-        AddCityController *addCity = [[AddCityController alloc] init];
-        [delegate.navController pushViewController:addCity animated:YES];
-    }
     [tv deselectRowAtIndexPath:indexPath animated:YES];
-}
-
-- (UITableViewCellEditingStyle)tableView:(UITableView *)tv editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (indexPath.row < cities.count) {
-        return UITableViewCellEditingStyleDelete;
-    } else {
-        return UITableViewCellEditingStyleInsert;
-    }
 }
 
 @end
