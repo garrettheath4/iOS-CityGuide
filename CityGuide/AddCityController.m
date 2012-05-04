d633edb06ccef225e04c6c280ba403272cafd736
Garrett Heath Koller <kollerg14@mail.wlu.edu>
2012-05-04 15:03:57 -0400

    Initial paste of keyboard fix code (needs fixing)

--- CityGuide/AddCityController.m
+++ CityGuide/AddCityController.m
@@ -81,10 +81,16 @@
     pickerController.allowsEditing = NO;
     pickerController.delegate = self;
     pickerController.sourceType = UIImagePickerControllerSourceTypeSavedPhotosAlbum;
+    
+    //Fix scrolling problem
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWasShown:) name:UIKeyboardDidShowNotification object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillBeHidden:) name:UIKeyboardWillHideNotification object:nil];
 }
 
 - (void)viewDidUnload
 {
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
@@ -93,6 +99,53 @@
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+#pragma mark UIKeyboard management Methods
+
+- (void)keyboardWasShown:(NSNotification*)aNotification {
+    
+    NSDictionary* info = [aNotification userInfo];
+    CGSize kbSize = [[info objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
+    
+    // get the keyboard height
+    double keyboardHeight = kbSize.height;
+    UIInterfaceOrientation currentOrientation = [[UIApplication sharedApplication] statusBarOrientation];
+    
+    // The calculations are little different for landscape mode.
+    if (UIInterfaceOrientationLandscapeLeft == currentOrientation ||UIInterfaceOrientationLandscapeRight == currentOrientation ) {
+        keyboardHeight = kbSize.width;
+    }
+    
+    // save the position of the scroll view, so that we can scroll it to its original position when keyboard disappears.
+    originalScrollOffset = scroller.contentOffset;
+    
+    CGPoint cp = [editView convertPoint:editView.bounds.origin toView:[UIApplicationsharedApplication].keyWindow.rootViewController.view];
+    
+    cp.y += editView.frame.size.height;
+    
+    CGRect bounds = [[UIScreen mainScreen] applicationFrame];
+    double sh = bounds.size.height;
+    
+    UIInterfaceOrientation currentOrientation = [[UIApplication sharedApplication] statusBarOrientation];
+    
+    if (UIInterfaceOrientationLandscapeLeft == currentOrientation ||UIInterfaceOrientationLandscapeRight == currentOrientation ) {
+        sh = bounds.size.width;
+    }
+    
+    // scroll if the keyboard is hiding the text view
+    if(cp.y > sh - keyboardHeight){
+        double sofset = cp.y - (sh - keyboardHeight);
+        CGPoint offset = scroller.contentOffset;
+        offset.y += sofset;
+        scroller.contentOffset = offset;
+    }
+}
+
+- (void)keyboardWillBeHidden:(NSNotification*)aNotification
+{
+    // restore the scroll position
+    scroller.contentOffset = originalScrollOffset;
 }
 
 #pragma mark UITableViewDataSource Methods
